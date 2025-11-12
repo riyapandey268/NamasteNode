6f56bab22b5f2bb96fb0bc60f244fd8cd97b325a
riyapandey268 <rouapandey2608@gmail.com>
2025-11-12 10:58:57 +0530

    exploring get post

--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
 const express = require ("express");
 
 const app = express();
-// request handler 
-app.use("/", (req, res) =>{
-    res.send ("Hello from Dashboard");
-})
+// request handler while working with req handlers order matters 
+
 app.use("/hello", (req, res) =>{
     res.send ("Hello from Another Server");
 })
+
 app.use("/test", (req, res) =>{
     res.send ("Hello from Server");
+})
+
+// app.use("/", (req, res) =>{
+//     res.send ("Hello from Dashboard");
+// })
+
+app.get("/user", (req, res) =>{
+  res.send( " Getting user data")
+})
+
+app.post("/user", (req, res) =>{
+    //Saving data to to Database
+    // console.log("save data to the database");
+    res.send("data saved successfully to the database");
+})
+app.delete("/user", (req, res) =>{
+    res.send ("Data Deleted successfully")
 })
 app.listen(3000, () =>{
     console.log("Server is Running on port 3000");
 });
 
-
+//http methods 
+//POST--> TO ADD A NEW USER
+//PATCH --> TO PARTIALLY UPDATE DATA
+//PUT --> TO UPDATE EXISTING DATA
+//GET --> TO GET ALL USERS
+//DELETE --> TO DELETE DATA
 
